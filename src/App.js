eec7e3d71255c05276fb3cdc47f169191d4df0b8
justinbeharry <jsbeharr@umich.edu>
2018-07-06 15:01:50 -0400

    Create WeatherBox card
    *Build a card containing weather data
    *Add momentjs library for date formatting

--- src/App.js
+++ src/App.js
@@ -9,11 +9,13 @@ import DetailBox from './DetailBox/DetailBox';
 class App extends Component {
 
   state = {
-    'temperature'   :   56,
-    'humidity'      :   80,
-    'pressure'      :   20.76,
-    'wind_speed'    :   3.5,
-    'wetness'       :   1020,
+    'weather_now'   : {
+      'temperature'   :   56,
+      'humidity'      :   80,
+      'pressure'      :   20.76,
+      'wind_speed'    :   3.5,
+      'wetness'       :   1020,
+    },
     'weather_image' :   cloudy_rain,
   };
 
@@ -29,12 +31,14 @@ class App extends Component {
             <img 
               id='git_lab' 
               src={git_lab_logo}
-              alt='git lab icon' /> 
+              alt='git lab icon'  /> 
           </a>
         </div>
-        <Banner image={this.state.weather_image}/>
+        <Banner 
+          image={this.state.weather_image}  />
         <div className="content">
-          <WeatherBox />
+          <WeatherBox
+            current_weather={this.state.weather_now}  />
         </div>
       </div>
     );
--- src/WeatherBox/WeatherBox.js
+++ src/WeatherBox/WeatherBox.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './WeatherBox.css';
+const moment = require('moment');
 
 const weather_box = (props) => {
+
+  const temperature = props.current_weather.temperature;
+  const humidity = props.current_weather.humidity;
+  const pressure = props.current_weather.pressure;
+  const wind_speed = props.current_weather.wind_speed;
+
   return (
     <div className='WeatherBox'>
-      <p>WeatherBox Component</p>
+      <div class='card'>
+        <header>
+          <p>{temperature}&deg;F</p>
+          <p>{moment().format('dddd')}</p>
+          <p>{moment().format('MMMM D, YYYY')}</p>
+        </header>
+          <p>Humidity: {humidity}%         </p>
+          <p>Pressure: {pressure} Hg       </p>
+          <p>Wind Speed: {wind_speed} m/s  </p>
+      </div>
     </div>
   );
 };
