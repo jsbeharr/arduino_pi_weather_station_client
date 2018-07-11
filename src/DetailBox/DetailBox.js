b2eb8e23c784810fd12910cc4e22e5d4d0334f24
justinbeharry <jsbeharr@umich.edu>
2018-07-10 22:31:25 -0400

    removed unecessary code

--- src/DetailBox/DetailBox.js
+++ src/DetailBox/DetailBox.js
@@ -1,17 +1,42 @@
 import React from 'react';
+import {
+  FlexibleXYPlot,
+  XAxis,
+  YAxis,
+  VerticalGridLines,
+  HorizontalGridLines,
+  LineMarkSeries,
+} from 'react-vis';
 
 const detail_box = (props) => {
   return (
     <div className='WeatherBox'>
       <div className='card'>
-        <header>
-          <p>DATA</p>
-          <p>DATA</p>
-          <p>DATA</p>
-        </header>
-          <p>DATA</p>
-          <p>DATA</p>
-          <p>DATA</p>
+          <FlexibleXYPlot
+            height={'260'}  >
+            <VerticalGridLines />
+            <HorizontalGridLines />
+            <XAxis title='x-axis' />
+            <YAxis title='y-axis'/>
+            <LineMarkSeries
+              className="linemark-series-example"
+              style={{
+                stroke: 'white'
+              }}
+              data={[
+                { x: 1, y: 10 },
+                { x: 2, y: 5 },
+                { x: 3, y: 15 }
+              ]} />
+            <LineMarkSeries
+              className="linemark-series-example-2"
+              curve={'curveMonotoneX'}
+              data={[
+                { x: 1, y: 11 },
+                { x: 1.5, y: 29 },
+                { x: 3, y: 7 }
+              ]} />
+          </FlexibleXYPlot>
       </div>
     </div>
   );
