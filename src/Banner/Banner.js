af2381b40a80e1c3efeafd53fbf9948258d3a31b
justinbeharry <jsbeharr@umich.edu>
2018-07-12 17:21:29 -0400

    Refactor css class and ids
    *Make banner title and location time be in class
    *Move around css containers

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -5,11 +5,11 @@ import Clock from 'react-live-clock';
 const banner = (props) => {
   return (
     <div className='Banner'>
-        <p id='title'>Arduino Pi Weather Station</p>
-        <div className='weather_img_container'>
+        <p id='title' className='banner_header'>Arduino Pi Weather Station</p>
+        <div id='weather_img_container'>
           <img src={props.image} alt='weather'/>
         </div>
-        <div id='location_time'>
+        <div id='location_time' className='banner_header'>
           <p>Ann Arbor, Michigan</p>
           <Clock format={'h:mm'} ticking={true} timezone={'US/Eastern'} />
         </div>
